2495584e357dd2cfc486c224829f09dc952fa2c7
Philip Dexter <philip.dexter@gmail.com>
2014-02-15 23:41:53 -0500

    Add number increment function and keybinding

--- config/global.el
+++ config/global.el
@@ -150,6 +150,24 @@
     ad-do-it))
 (ad-activate 'linum-update)
 
+;; increment numbers
+(defun increment-number-decimal (&optional arg)
+  "Increment the number forward from point by 'arg'."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "0123456789")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                 answer)))))))
+(global-set-key (kbd "C-c +") 'increment-number-decimal)
+
 ;; global key bindings
 
 (define-key global-map (kbd "C-c C-s") 'ace-jump-mode)
