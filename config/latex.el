7d86fdbd01cfbd61346e54717c093415a4b16a8c
Philip Dexter <philip.dexter@gmail.com>
2014-03-15 19:58:48 -0400

    Add latex paragraph fill to break on punctuation

--- config/latex.el
+++ config/latex.el
@@ -2,3 +2,14 @@
 
 ;; unset C-c C-s so we can use it in ace-jump
 (define-key latex-mode-map (kbd "C-c C-s") nil)
+
+;; fill paragraph using punctuation
+(defun fill-using-punctuation ()
+  (interactive)
+  (save-excursion
+    (progn (mark-paragraph)
+	   (let ((begin (region-beginning))
+		 (end (region-end)))
+	     (replace-regexp "\\([.,?:;!]\\)\s+" "\\1
+" nil begin end)))))
+(define-key latex-mode-map (kbd "C-c q") 'fill-using-punctuation)
