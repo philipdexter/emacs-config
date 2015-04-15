1e7f17b1c733a01e744f866c7497dd1051320500
Philip Dexter <philip.dexter@gmail.com>
2015-04-15 13:04:23 -0400

    Add base library to idris

--- config/idris.el
+++ config/idris.el
@@ -1,2 +1,2 @@
 (setq idris-interpreter-path "/home/philip/projects/idris-dev/dist/build/idris/idris")
-(setq idris-interpreter-flags '("-i" "/home/philip/projects/idris-dev/libs/prelude"))
+(setq idris-interpreter-flags '("-i" "/home/philip/projects/idris-dev/libs/prelude" "-i" "/home/philip/projects/idris-dev/libs/base"))
