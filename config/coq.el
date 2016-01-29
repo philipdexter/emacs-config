a52ebab6ef7524245f75403c1c214c4da597b53c
Philip Dexter <philip.dexter@gmail.com>
2016-01-29 11:44:39 -0500

    Add company-coq

--- config/coq.el
+++ config/coq.el
@@ -1,9 +1,30 @@
+(defvar coq-packages
+  '(yasnippet
+    dash
+    company
+    math-symbol-lists
+    company-math
+    company-coq))
+
 (defun coq-find-hook()
   (let ((fn (buffer-file-name)))
     (when (string-match "\\.v$" fn)
       (load-file "/home/philip/.emacs.d/packages/ProofGeneral/generic/proof-site.el")
+      (loop for name in coq-packages
+	    do (progn (unless (fboundp name)
+			(add-to-list 'load-path
+				     (concat "~/.emacs.d/"
+					     "packages/"
+					     (symbol-name name)))
+			(require name))))
       (coq-mode)
-      (define-key coq-mode-map (kbd "C-c RET") 'proof-goto-point))))
+      (company-coq-mode)
+      (setq company-coq-features/prettify-symbols-in-terminals t)
+      (proof-electric-terminator-toggle)
+      (define-key coq-mode-map (kbd "M-p") 'proof-goto-command-start)
+      (define-key coq-mode-map (kbd "M-n") 'proof-goto-command-end)
+      (define-key coq-mode-map (kbd "C-c RET") 'proof-goto-point)
+      (define-key coq-mode-map (kbd "M-RET") 'proof-goto-point))))
 (add-hook 'find-file-hooks 'coq-find-hook)
 
 ; (add-to-list 'auto-mode-alist '("\\.v$" . coq-mode))
