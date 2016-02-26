d2859509e45340596c55b0c6f31c69faa9dd1009
Philip Dexter <philip.dexter@gmail.com>
2016-02-26 09:09:07 -0500

    Fix latex hook

--- config/latex.el
+++ config/latex.el
@@ -1,8 +1,5 @@
 (require 'tex-mode)
 
-;; unset C-c C-s so we can use it in ace-jump
-(define-key latex-mode-map (kbd "C-c C-s") nil)
-
 ;; fill paragraph using punctuation
 (defun fill-using-punctuation ()
   (interactive)
@@ -13,3 +10,9 @@
 	     (replace-regexp "\\([.,?:;!]\\)\s+" "\\1
 " nil begin end)))))
 (define-key tex-mode-map (kbd "C-c q") 'fill-using-punctuation)
+
+(add-hook 'tex-mode-hook
+	  (lambda () (progn (add-to-list 'load-path "~/.emacs.d/packages/auctex")
+			    (load "auctex.el" nil t t)
+			    (load "preview-latex.el" nil t t)
+			    (LaTeX-mode))))
