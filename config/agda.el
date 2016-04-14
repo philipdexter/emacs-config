5cbb09f4b45876a1a65e47b24effa0be7e19b848
Philip Dexter <philip.dexter@gmail.com>
2016-04-14 10:57:55 -0400

    Reorganize agda and coq configs

--- config/agda.el
+++ config/agda.el
@@ -1,8 +1,15 @@
-(defun agda-find-hook()
+(defvar my/agda-loaded nil)
+
+(defun my/load-agda ()
+  (setq agda2-program-args '("--include-path=/home/philip/projects/agda/std-lib/src" "--include-path=."))
+  (load-file (let ((coding-system-for-read 'utf-8))
+	       (shell-command-to-string "agda-mode locate")))
+  (setq my/agda-loaded t))
+
+(defun my/agda-find-hook()
   (let ((fn (buffer-file-name)))
     (when (string-match "\\.agda$" fn)
-      (setq agda2-program-args '("--include-path=/home/philip/projects/agda/std-lib/src" "--include-path=."))
-      (load-file (let ((coding-system-for-read 'utf-8))
-                      (shell-command-to-string "agda-mode locate")))
+      (unless my/agda-loaded (my/load-agda))
       (agda2-mode))))
-(add-hook 'find-file-hooks 'agda-find-hook)
+
+(add-hook 'find-file-hooks 'my/agda-find-hook)
--- config/coq.el
+++ config/coq.el
@@ -1,44 +1,49 @@
-(defvar coq-packages
-  '(yasnippet
-    dash
-    company
-    math-symbol-lists
-    company-math
-    company-coq))
+(defvar my/coq-loaded nil)
 
-(defun proof-goto-next ()
+(defun my/load-coq ()
+  (let ((coq-packages '(yasnippet
+			dash
+			company
+			math-symbol-lists
+			company-math
+			company-coq)))
+    (load-file "/home/philip/.emacs.d/packages/ProofGeneral/generic/proof-site.el")
+    (loop for name in coq-packages
+	  do (unless (fboundp name)
+	       (add-to-list 'load-path
+			    (concat
+			     "~/.emacs.d/"
+			     "packages/"
+			     (symbol-name name)))
+	       (require name)))
+    (setq overlay-arrow-string "")
+    (coq-mode)
+    (company-coq-mode)
+    (put 'company-coq-fold 'disabled nil)
+    (setq company-coq-features/prettify-symbols-in-terminals t)
+    (set-face-attribute 'proof-locked-face nil :background "#1c1c1c" :underline nil)
+    (proof-electric-terminator-enable 1)
+    (setq coq-prog-args '("-emacs-U" "-R" "/home/philip/coq/cpdt/src" "Cpdt"))
+    (define-key coq-mode-map (kbd "C-c C-s") nil)
+    (define-key coq-mode-map (kbd "C-c C-_") 'company-coq-fold)
+    (define-key coq-mode-map (kbd "M-p") 'proof-undo-last-successful-command)
+    (define-key coq-mode-map (kbd "M-n") 'my/proof-goto-next)
+    (define-key proof-mode-map (kbd "M-p") 'proof-undo-last-successful-command)
+    (define-key proof-mode-map (kbd "M-n") 'proof-goto-next)
+    (define-key coq-mode-map (kbd "C-c C-a d") 'company-coq-toggle-definition-overlay)
+    (define-key coq-mode-map (kbd "M-RET") 'proof-goto-point)
+    (setq my/coq-loaded t)))
+
+(defun my/proof-goto-next ()
   (interactive)
-  (progn (proof-goto-end-of-locked)
-         (proof-goto-command-end)
-         (proof-goto-command-end)
-         (company-coq-proof-goto-point)))
+  (proof-goto-end-of-locked)
+  (proof-goto-command-end)
+  (proof-goto-command-end)
+  (company-coq-proof-goto-point))
 
-(defun coq-find-hook()
+(defun my/coq-find-hook()
   (let ((fn (buffer-file-name)))
     (when (string-match "\\.v$" fn)
-      (load-file "/home/philip/.emacs.d/packages/ProofGeneral/generic/proof-site.el")
-      (loop for name in coq-packages
-            do (progn (unless (fboundp name)
-                        (add-to-list 'load-path
-                                     (concat "~/.emacs.d/"
-                                             "packages/"
-                                             (symbol-name name)))
-                        (require name))))
-      (coq-mode)
-      (setq overlay-arrow-string "")
-      (company-coq-mode)
-      (put #'company-coq-fold 'disabled nil)
-      (setq company-coq-features/prettify-symbols-in-terminals t)
-      (set-face-attribute 'proof-locked-face nil :background "#1c1c1c" :underline nil)
-      (proof-electric-terminator-enable 1)
-      (setq coq-prog-args '("-emacs-U" "-R" "/home/philip/coq/cpdt/src" "Cpdt"))
-      (define-key coq-mode-map (kbd "C-c C-s") nil)
-      (define-key coq-mode-map (kbd "C-c C-_") 'company-coq-fold)
-      (define-key coq-mode-map (kbd "M-p") 'proof-undo-last-successful-command)
-      (define-key coq-mode-map (kbd "M-n") 'proof-goto-next)
-      (define-key proof-mode-map (kbd "M-p") 'proof-undo-last-successful-command)
-      (define-key proof-mode-map (kbd "M-n") 'proof-goto-next)
-      (define-key coq-mode-map (kbd "C-c RET") 'proof-goto-point)
-      (define-key coq-mode-map (kbd "C-c C-a d") 'company-coq-toggle-definition-overlay)
-      (define-key coq-mode-map (kbd "M-RET") 'proof-goto-point))))
-(add-hook 'find-file-hooks 'coq-find-hook)
+      (unless my/coq-loaded (my/load-coq)))))
+
+(add-hook 'find-file-hooks 'my/coq-find-hook)
