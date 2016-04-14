d4233b3c5138dd0dae140d2a0c64252569c6dcee
Philip Dexter <philip.dexter@gmail.com>
2016-04-14 12:06:57 -0400

    Update idris path

--- config/idris.el
+++ config/idris.el
@@ -1,2 +1,1 @@
-(setq idris-interpreter-path "/home/philip/projects/idris-dev/dist/build/idris/idris")
-(setq idris-interpreter-flags '("-i" "/home/philip/projects/idris-dev/libs/prelude" "-i" "/home/philip/projects/idris-dev/libs/base"))
+(setq idris-interpreter-flags '("-i" "/home/philip/projects/idris/libs/prelude" "-i" "/home/philip/projects/idris/libs/base"))
