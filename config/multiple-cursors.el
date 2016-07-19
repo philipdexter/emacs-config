1c171544fcb79f3af1c45f28dfdb923a38262034
Philip Dexter <philip.dexter@gmail.com>
2016-07-19 15:26:47 +0100

    Add multiple-cursor keybindings for next- and previous-like-this

--- config/multiple-cursors.el
+++ config/multiple-cursors.el
@@ -1,2 +1,4 @@
 (global-set-key (kbd "C-M-@") 'mc/mark-all-dwim)
 (global-set-key (kbd "C-x r a") 'set-rectangular-region-anchor)
+(global-set-key (kbd "C-M-n") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-M-p") 'mc/mark-previous-like-this)
