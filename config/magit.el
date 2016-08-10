00fad12784007c1b5fff76783fd016afe775ee01
Philip Dexter <philip.dexter@gmail.com>
2016-08-10 20:55:22 +0100

    Always show magit-status on magit binding and load if necessary

--- config/magit.el
+++ config/magit.el
@@ -1,8 +1,17 @@
+(defvar my/magit-loaded nil)
+
 (add-to-list 'load-path "~/.emacs.d/packages/magit/lisp")
 (add-to-list 'load-path "~/.emacs.d/packages/with-editor")
 (setq git-commit-fill-column nil)
 (setq git-commit-summary-max-length 70)
 (setq magit-auto-revert-mode nil)
 
-(defun load-magit () (interactive) (require 'magit))
+(defun load-magit ()
+  (interactive)
+  (if my/magit-loaded
+      (magit-status)
+    (progn
+      (require 'magit)
+      (setq my/magit-loaded t)
+      (load-magit))))
 (define-key global-map (kbd "C-x C-a l") 'load-magit)
