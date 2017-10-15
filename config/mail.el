7168d83ae3a9295cde3eca4fd0383c63afa8a299
Philip Dexter <philip.dexter@gmail.com>
2017-10-15 22:40:52 +0200

    Remove old mail config

--- config/mail.el
+++ config/mail.el
@@ -11,11 +11,7 @@
 	       (nnimap-stream ssl)))
 
 (setq gnus-secondary-select-methods
-      '((nnimap "phfilip"
-		(nnimap-address "mail.phfilip.com")
-		(nnimap-server-port 143)
-		(nnimap-stream starttls))
-	(nnimap "runbox"
+      '((nnimap "runbox"
 		(nnimap-address "mail.runbox.com")
 		(nnimap-server-port 993)
 		(nnimap-stream ssl))))
