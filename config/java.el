aac7b97ba30e964d1b1a7f4febd72131e6125c89
Philip Dexter <philip.dexter@gmail.com>
2018-03-30 13:08:43 +0200

    Add ant-at-git command

--- config/java.el
+++ config/java.el
@@ -2,3 +2,10 @@
 			    (setq c-basic-offset 4
 				  tab-width 4
 				  indent-tabs-mode nil)))
+
+(defun ant-at-git ()
+  (interactive)
+  (let ((old-dir default-directory))
+    (cd (locate-dominating-file "." ".git"))
+    (compile "ant build")
+    (cd old-dir)))
