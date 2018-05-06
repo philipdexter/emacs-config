f9be09e14bc8465a62cf5494e78f3f40da28bbd2
Philip Dexter <philip.dexter@gmail.com>
2018-05-06 15:44:56 +0200

    Load asciidoc automatically

--- config/asciidoc.el
+++ config/asciidoc.el
@@ -1,3 +1,5 @@
+(defvar my/asciidoc-loaded nil)
+
 (defun my/load-asciidoc ()
   (interactive)
 
@@ -6,9 +8,18 @@
   (require 'adoc-mode)
   (adoc-mode)
 
+  ; (setq my/asciidoc-loaded t)
+
   ;; (set-face-attribute 'markup-list-face nil :background "yellow" :foreground "black")
   ;; (set-face-attribute 'markup-meta-hide-face nil :background "black" :foreground "yellow")
   ;; (set-face-attribute 'markup-typewriter-face nil :background "black" :foreground "bright red")
   ;; (set-face-attribute 'markup-meta-face nil :background "black" :foreground "bright red")
 
-  (global-set-key (kbd "C-c C-c") (lambda () (interactive) (compile "asciidoctor -r asciidoctor-diagram meeting.txt"))))
+  (global-set-key (kbd "C-c C-c") (lambda () (interactive) (compile (concat "asciidoctor -r asciidoctor-diagram " (buffer-name))))))
+
+(defun my/asciidoc-find-hook ()
+  (let ((fn (buffer-file-name)))
+    (when (string-match "\\.txt" fn)
+      (unless my/asciidoc-loaded (my/load-asciidoc)))))
+
+(add-hook 'find-file-hooks 'my/asciidoc-find-hook)
